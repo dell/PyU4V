29122b96b377d63538fdb5637654c60d2903a021
rawstorage <paule.martin@dell.com>
2017-02-10 11:10:30 +0000

    moved function calls in to main and automatically create linksg name for standard naming

--- link_snapshot.py
+++ link_snapshot.py
@@ -30,19 +30,20 @@ from PyU4V.rest_univmax import rest_functions
 PARSER = argparse.ArgumentParser(description='This python scrtipt is a basic VMAX REST recipe used for creating and provisioning storage for an Oracle Database.')
 RFLAGS = PARSER.add_argument_group('Required arguments')
 RFLAGS.add_argument('-sg', required=True, help='Storage group name, typically the application name e.g. REST_TEST_SG')
-RFLAGS.add_argument('-lnsg', required=True, help='Storage group name you want to link to, if name does not exist the storage group will be greated with all required volumes')
 ARGS = PARSER.parse_args()
 
 #Variables are initiated to append REST to the Storage Group and Initiator this can all be customized to match your individual
 #requirements
 
 sg_id = ARGS.sg
-ln_sg_id=ARGS.lnsg
+ln_sg_id=sg_id+("_LNK")
 ru = rest_functions()
-mysnap = ru.set_snapshot_id(sg_id)
-print("You Chose Snap %s" % mysnap)
 mvname=ln_sg_id +("_MV")
 
-ru.link_gen_snapsthot_83(sg_id,mysnap,generation=0,link_sg_name=ln_sg_id)  #Currently getting a timeout issue if storage group doesn't already exist, this has been addressed in 84.  Method will be updated to use 84 endpoint when GA
-ru.create_masking_view_existing_components(port_group_name="REST_Paul_PG",masking_view_name=mvname, host_name="REST_Paul_IG", storage_group_name=ln_sg_id)
+def main ():
+    mysnap = ru.set_snapshot_id(sg_id)
+    print("You Chose Snap %s" % mysnap)
+    ru.link_gen_snapsthot_83(sg_id,mysnap,generation=0,link_sg_name=ln_sg_id)
+    ru.create_masking_view_existing_components(port_group_name="cse_pg",masking_view_name=mvname, storage_group_name=ln_sg_id, host_name="esx144_IG")
 
+main()
