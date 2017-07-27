aafe85463db03866a5fecbca3e2efbce25f07212
rawstorage <paule.martin@dell.com>
2017-07-27 12:10:10 +0100

    Updated PortGroup methods to use new format.

--- PyU4V/rest_univmax2.py
+++ PyU4V/rest_univmax2.py
@@ -334,7 +334,7 @@ class RestFunctions:
         version = self.U4V_VERSION
         target_uri = self._build_uri(array, category, resource_type,
                                      resource_name, version=version)
-        status_code, message = self.request(target_uri, DELETE,
+        message, status_code = self.request(target_uri, DELETE,
                                             request_object=payload,
                                             params=params, stream=False)
         operation = 'delete %(res)s resource' % {'res': resource_type}
@@ -1115,13 +1115,11 @@ class RestFunctions:
         :param port_id: the port id
         :return: dict, status_code
         """
-        target_uri = ("/sloprovisioning/symmetrix/%s/portgroup"
-                      % self.array_id)
-        pg_payload = ({"portGroupId": portgroup_id,
+        payload = ({"portGroupId": portgroup_id,
                        "symmetrixPortKey": [{"directorId": director_id,
                                              "portId": port_id}]})
-        return self.rest_client.rest_request(target_uri, POST,
-                                             request_object=pg_payload)
+        return self.create_resource(
+            self.array_id, SLOPROVISIONING, 'portgroup', payload)
 
     def create_multiport_portgroup(self, portgroup_id, ports):
         """Create a new portgroup.
@@ -1131,12 +1129,11 @@ class RestFunctions:
                                             "portId": port_id}
         :return: dict, status_code
         """
-        target_uri = ("/sloprovisioning/symmetrix/%s/portgroup"
-                      % self.array_id)
-        pg_payload = ({"portGroupId": portgroup_id,
+        payload = ({"portGroupId": portgroup_id,
                        "symmetrixPortKey": ports})
-        return self.rest_client.rest_request(target_uri, POST,
-                                             request_object=pg_payload)
+        return self.create_resource(
+            self.array_id, SLOPROVISIONING, 'portgroup', payload)
+
 
     def create_portgroup_from_file(self, file_name, portgroup_id):
         """Given a file with director:port pairs, create a portgroup.
