93f90abcbfa96402c776ea58155ae8eedbb58538
stackc2 <ciara.stacke@emc.com>
2018-03-07 14:57:29 +0000

    Fix name in example.

--- ProgrammersGuideExamples/HelloUnisphere.py
+++ ProgrammersGuideExamples/HelloUnisphere.py
@@ -2,20 +2,19 @@
 
 from PyU4V import U4VConn
 
-conn = U4VConn(u4v_version='90', server_ip='IPADDRESS',port=8443,
-               verify=False,username='smc',password='smc')
+conn = U4VConn(u4v_version='90', server_ip='IPADDRESS', port=8443,
+               verify=False, username='smc', password='smc')
 
-#Make a get call to find the version
-version=conn.common.get_uni_version()
+# Make a get call to find the version
+version = conn.common.get_uni_version()
 
-#Get a list of managed arrays
-array_list=conn.common.get_array_list()
+# Get a list of managed arrays
+array_list = conn.common.get_array_list()
 
 
-
-print ("Congratulations you have just connected to Unisphere for "
-       "PowerMax your Version is ", version[1])
-print ("This Unisphere instance manages the following arrays", array_list)
+print("Congratulations you have just connected to Unisphere for "
+      "VMAX your Version is ", version[1])
+print("This Unisphere instance manages the following arrays", array_list)
 
 local_array_list = []
 
@@ -25,6 +24,5 @@ for i in array_list:
         local_array_list.append(i)
 
 
-
-print ("The following arrays are local to this Unisphere instance",
-       local_array_list)
+print("The following arrays are local to this Unisphere instance",
+      local_array_list)
