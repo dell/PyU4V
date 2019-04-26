1ffc868f52f51a0d20c266e2780b33b48de13f51
rawstorage <paule.martin@dell.com>
2019-04-26 10:17:01 -0400

    Update versioning in __init__.py and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PyU4V',
-      version='3.0.0.11',
+      version='3.0.0.12',
       url='https://github.com/MichaelMcAleer/PyU4V/',
       author='Dell Inc. or its subsidiaries',
       author_email='Michael.Mcaleer@dell.com',
