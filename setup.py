f73f4d7fd9ff184ab6c9ae94684144691a4fdcdc
MichaelMcAleer <Michael.Mcaleer@emc.com>
2019-08-02 14:22:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='PyU4V',
-      version='3.0.0.16',
+      version='3.0.0.17',
       url='https://github.com/MichaelMcAleer/PyU4V/',
       author='Dell Inc. or its subsidiaries',
       author_email='Michael.Mcaleer@dell.com',
