47b6be3e00216c505761f06048f2ee87a7740a7b
helenwalsh <helen.walsh@emc.com>
2019-08-08 15:06:23 +0100

    Regression fix
    Regression fix, removal of line of code in 3.0.0.15

--- PyU4V/__init__.py
+++ PyU4V/__init__.py
@@ -6,8 +6,9 @@
 #       /             |
 """__init__.py."""
 
+from .univmax_conn import U4VConn  # noqa: F401
 __title__ = 'pyu4v'
 __version__ = '3.0.0.17'
-__author__ = 'Dell EMC or its subsidieries'
+__author__ = 'Dell EMC or it\'s subsidiaries'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Dell EMC Inc'
