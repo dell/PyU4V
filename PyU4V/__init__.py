e59c43c73689c64a4c28fa6e39783f13f606a3d0
helenwalsh <helen.walsh@emc.com>
2019-08-08 15:14:32 +0100

    Fix regression

--- PyU4V/__init__.py
+++ PyU4V/__init__.py
@@ -9,6 +9,6 @@
 from .univmax_conn import U4VConn  # noqa: F401
 __title__ = 'pyu4v'
 __version__ = '3.0.0.17'
-__author__ = 'Dell EMC or it\'s subsidiaries'
+__author__ = 'Dell EMC or its subsidiaries'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Dell EMC Inc'
