8b34211e2c70e233166fe4023a4eeb1376d7a568
Martin, Paul <paule.martin@emc.com>
2024-11-01 12:16:04 +0000

    pep8

--- PyU4V/scripts/replace_version.py
+++ PyU4V/scripts/replace_version.py
@@ -4,20 +4,10 @@ from version import (VERSION, API_VERSION,
                      UNI_VERSION, MAJOR_VERSION)
 import shutil
 
-version_file_list = ['../../README.rst', '../../index.rst','../../setup.py',
-                     '../../docs/source/installation.rst',
-                     '../../docs/source/index.rst']
 
-files_to_copy = [('../../README_template.rst',
-                       '../../README.rst'),
-                      ('../../index_template.rst','../../index.rst'),
-                      ('../../setup_template.py', '../../setup.py' ),
-                     ('../../docs/source/installation_template.rst',
-                      '../../docs/source/installation.rst'),
-                     ('../../docs/source/index_template.rst',
-                      '../../docs/source/index.rst')]
-for original_file, new_file in files_to_copy:
+def copy_file(original_file, new_file):
     shutil.copyfile(original_file, new_file)
+
 
 def update_version(version_file_list):
     for filename in version_file_list:
@@ -29,5 +19,29 @@ def update_version(version_file_list):
                 line = line.replace('{api_version}', API_VERSION)
                 print(line, end='')
 
-update_version(version_file_list)
 
+def main():
+    version_files = [
+        '../../README.rst',
+        '../../index.rst',
+        '../../setup.py',
+        '../../docs/source/installation.rst',
+        '../../docs/source/index.rst'
+    ]
+
+    original_files = [
+        '../../README_template.rst',
+        '../../index_template.rst',
+        '../../setup_template.py',
+        '../../docs/source/installation_template.rst',
+        '../../docs/source/index_template.rst'
+    ]
+
+    for original, new in zip(original_files, version_files):
+        copy_file(original, new)
+
+    update_version(version_files)
+
+
+if __name__ == '__main__':
+    main()
